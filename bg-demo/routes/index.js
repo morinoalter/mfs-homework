a5fd83f2f41b99ab26e2f8140161b75aa2bc8faa
morino <736252242@qq.com>
2020-04-26 06:50:27 +0800

    2020/4/26

---
+++ bg-demo/routes/index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.render('index', { title: 'Express' });
+});
+
+router.get("/json", function(req, res) {
+    res.json({ data: "hello json" });
+});
+
+
+module.exports = router;
